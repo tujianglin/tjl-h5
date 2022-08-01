10fc93ef3a05b15c16147911be2f6e495643e763
tujianglin <jianglin.tu@wimisoft.com>
2022-08-01 14:26:46 +0800

    feat: build-html

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -4,13 +4,16 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import legacy from '@vitejs/plugin-legacy';
 
 import { configStyleImportPlugin } from './styleImport';
+import { configHtmlPlugin } from './html';
 
-export function createVitePlugin(viteEnv: ViteEnv, isBuild?: boolean) {
+export function createVitePlugin(viteEnv: ViteEnv, isBuild: boolean) {
   const { VITE_LEGACY } = viteEnv;
 
   const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()];
 
   VITE_LEGACY && isBuild && vitePlugins.push(legacy());
+
+  vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
 
   vitePlugins.push(configStyleImportPlugin());
 
